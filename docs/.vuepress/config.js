0288c5d89b53fff4a668fe5c8f9d4fb6fd5009db
Mr-CHANX <48688440+Mr-CHANX@users.noreply.github.com>
2020-05-09 23:14:50 +0800

    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', href: '/favicon.ico' }],
         ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
-        ['script', { type: 'text/javascript', src: '/assets/baidu.js' }],
+        ['script', { type: 'text/javascript', src: '/assets/js/baidu.js' }],
     ],
     theme: 'reco',
     themeConfig: {
